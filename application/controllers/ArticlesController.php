74c4178ba802aa6c300ca0b17fbb2cda4b77b719
Evgvakh <evgvakh@gmail.com>
2023-02-03 08:58:13 +0100

    changes made

--- application/controllers/ArticlesController.php
+++ application/controllers/ArticlesController.php
@@ -4,7 +4,6 @@ require_once 'application/core/Controller.php';
 require_once 'application/core/View.php';
 class ArticlesController extends Controller
 {
-    
     public function showAction()
     {
         $data = $this->model->getData();          
