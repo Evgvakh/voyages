0a99f6064677c0ea40a96e75401f83918c5dfcbf
Evgvakh <evgvakh@gmail.com>
2023-02-06 09:00:24 +0100

    0602 matin

--- application/controllers/ArticlesController.php
+++ application/controllers/ArticlesController.php
@@ -13,7 +13,7 @@ class ArticlesController extends Controller
 
     public function showOneAction()
     {
-        echo '</br> ID_ARTICLE at ArticlesController: ' . $this->id_article . 'it has type - ', gettype($this->id_article);
+        echo '</br> ID_ARTICLE at ArticlesController: ' . $this->id_article . ', it has type - ', gettype($this->id_article);
         $data = $this->model->getOne($this->id_article);
         $comment_data = $this->model->getComments($this->id_article);
         $content = 'application/views/' . $this->route . '/' . $this->action . '.php';
